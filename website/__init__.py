36380ad88b7acdec7957a5eb7f86f7463813e207
Ragib Al Asad <ragibalasad@gmail.com>
2021-07-01 13:55:57 +0600

    Initialized db models

--- website/__init__.py
+++ website/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
+DB_NAME = 'database.db'
 
 def create_app():
     # Initialize app
     app = Flask(__name__)
     app.config['SECRET_KEY'] = '$rbn'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     # Register external routes as blueprints
     from .views import views
